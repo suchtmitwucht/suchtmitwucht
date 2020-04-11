0c47e4ef2bc61dfa5593285162d698c1ce9ca393
Jakob Meier <comcloudway@ccw.icu>
2020-04-11 10:32:37 +0200

    started episode view

--- index.js
+++ index.js
@@ -16,15 +16,99 @@ const cors = require('cors')
 
 app.get(["/:id","*"],(req,res)=>{
     if(req.params.id==undefined) req.params.id=""
-    fs.exists(path.join(__dirname,"public/podcasts",req.params.id,"info.json"),(exists)=>{
-        if(exists) {
-            //Podcasts Exists
-            res.send(req.params.id)
-        } else {
-            //Show home
-            res.sendFile(path.join(__dirname,"public","index.html"))
+    let buff = new Buffer(req.params.id,"base64");
+            let text = buff.toString('ascii')
+            var c = 0;
+            let ep = require(path.join(__dirname,"public","database","episodes.json"))
+            for(let episode of ep.episodes) {
+                if(text==episode.id) {
+                    //episode found
+                    c = 1;
+                    
+//Do sth.
+
+fs.readFile(path.join(__dirname,"public/episode-template","index.html"),"utf-8",(err,cont)=>{
+    if(err!=undefined) {
+        res.send("Error")
+    } else {
+        let parts = cont.split("!split!");
+
+//Create html presets
+let audio = ""
+let main = "";
+for (let file of episode.files) {
+    if(file.main) main=file.src
+    audio += `
+    <div class="item" onclick="document.getElementById('player-src').src='${file.src}'">
+    <p class="name">${file.name} ᐅ</p>
+    </div>
+    `
+}
+
+let team = ""
+for (let memb of episode.team) {
+    let main =  `
+    <div class="item">
+    <p class="name">${memb.name}</p>
+    `
+    for(let task of memb.work) {
+        main+=`
+        <p class="task">${task}</p>
+        `
+    }
+    team+=main+"</div>"
+}
+
+let src = ""
+for (let q of episode.src) {
+    src+=`
+    <div class="item">
+    <p class="left">Name:</p>
+    <p class="right">${q.name}</p>
+    <p class="left">Typ: </p>
+        <p class="right">${q.category}</p>
+    <p class="left">Künstler:</p>
+        <p class="right">${q.artist}</p>
+    <p class="left">Quelle:</p>
+        <a class="right" href="${q.src}">${q.src}</a>
+    <p class="left">Lizenz:</p>
+        <a class="right">${q.license}</a>
+    <p class="left">Unser Nutzen:</p>
+        <p class="right">${q.use}</p>
+    </div>
+    `
+}
+
+//inject html
+        let resp = ""
+        let data = [
+            episode.title,
+            episode.title,
+            episode.desc,
+            audio,
+            team,
+            src,
+            main,
+            episode.icon,
+            ""
+        ]
+        for (let p in parts) {
+                resp+=parts[p]+data[p]
+            
+            
         }
-    })
+        res.send(resp)
+    }
+})
+
+                    break;
+                }
+            }
+            if(c==0) {
+                //no episode found
+                res.sendFile(path.join(__dirname,"public","index.html"))
+            }
+   
 })
 
  app.listen(port,()=>{
---
+++ public/episode-template/src/index.js
@@ -0,0 +1,68 @@
+/*
+Useless insider
+*/
+console.log(`%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`, "font-family:monospace")
+
+const THREE = window.THREE = require("three");
+
+/*
+THREE.js 3D Background System
+*/
+var vars = {
+    fov: 75,
+    cameraZ: 0
+}
+
+vars.width = 2 * (Math.tan(vars.fov / 2 * Math.PI / 180) * vars.cameraZ)
+
+var loader = new THREE.TextureLoader();
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(vars.fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+camera.position.z = vars.cameraZ
+
+var light = new THREE.AmbientLight(0x000000)
+scene.add(light)
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+renderer.domElement.className = "viewport"
+
+const run = () => {
+    requestAnimationFrame(run)
+
+    renderer.render(scene, camera)
+}
+var mesh = undefined;
+loader.load('https://comcloudway.github.io/suchtmitwucht/public/res/pan.jpg', function (texture) {
+
+    var geometry = new THREE.SphereGeometry(vars.width, 20, 20);
+
+    var material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.y = 2 + window.scrollY / 400 - 2
+
+    scene.add(mesh)
+    run()
+});
+
+window.addEventListener("scroll", () => {
+    if (mesh != undefined) {
+        mesh.rotation.y = 2 + window.scrollY / 400 - 2
+    }
+
+})
+window.addEventListener("resize", () => {
+    location.reload()
+})
